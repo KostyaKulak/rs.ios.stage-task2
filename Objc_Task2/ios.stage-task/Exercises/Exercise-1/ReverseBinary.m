dad0168ae45ff527e8deb60970c88be6fd35a5ca
Kanstantsin Kulak <kkulak24@gmail.com>
2021-05-17 16:47:40 +0200

    feat: complete task 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 reverseNum = 0;
+    for (int i = 0; i < 8; i++) {
+        UInt8 tempOne = (UInt8) (n & (1 << i));
+        if (tempOne) {
+            reverseNum |= (1 << ((8 - 1) - i));
+        }
+    }
+    return reverseNum;
 }
